f320f55eaa008d6ca6b4754d1e2b7c988e38ab8e
bhujel12 <71912832+bhujel12@users.noreply.github.com>
2022-06-09 01:32:20 -0400

    CollisionAdded
    Walls and random object in center change color when Boi (player) collides with them

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -9,15 +9,26 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("What's up b*tch");
+        Debug.Log("Welcome to the End");
+        Debug.Log("Just kidding LOL");
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-
         transform.Translate(xValue, 0, zValue);
     }
 }
